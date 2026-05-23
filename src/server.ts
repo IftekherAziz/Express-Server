d3383c6b9a76b52473cb475e354f08b7eb0a5210
Iftekher Aziz <azizchy02@gmail.com>
2026-05-23 16:31:20 +0200

    Creating first User with POST method

--- src/server.ts
+++ src/server.ts
@@ -43,12 +43,24 @@ app.get('/', (req: Request, res: Response) => {
 
 // Endpoint to handle POST requests
 app.post('/', async (req: Request, res: Response) => {
-    // console.log(req.body);
-    const {name,age} = req.body;
+    const {name,email,password,age} = req.body;
+    try {
+        const result = await pool.query(`
+        INSERT INTO users (name, email, password, age) VALUES ($1, $2, $3, $4) RETURNING *
+        `, [name, email, password, age]
+    )
+    console.log(result);
     res.status(201).json({
         message: 'Data received successfully',
-        data: {name,age}
+        data: result.rows[0]
     });
+    } 
+    catch (error: any) {
+        res.status(500).json({
+            message: error.message,
+            error: error,
+        });
+    }
 });
 
 // Start the server
