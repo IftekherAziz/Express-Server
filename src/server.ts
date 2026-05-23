61df13282804cab63cf48bcf72b27b9a5a631f0a
Iftekher Aziz <azizchy02@gmail.com>
2026-05-23 15:16:34 +0200

    Learned the express Request and Response

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
-import express from 'express'
-const app = express()
+import express, { type Application, type Request, type Response } from 'express'
+const app: Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Aziz!')
+app.use(express.json()) // Middleware to parse JSON bodies
+
+app.get('/', (req: Request, res: Response) => {
+  // res.send('Hello World!')
+  res.status(200).json({
+    message: 'Hello World!'
+  })
+})  
+
+app.post('/', async (req: Request, res: Response) => {
+    console.log(req.body);
+
 })
 
 app.listen(port, () => {
