4468d4228a4c8537aa1404f29f2ee84027885560
Iftekher Aziz <azizchy02@gmail.com>
2026-05-23 15:42:33 +0200

    Setting Up Postgres with Neon Serverless Cloud

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,34 @@
 import express, { type Application, type Request, type Response } from 'express'
+import {Pool} from 'pg'
+
 const app: Application = express()
 const port = 3000
 
 app.use(express.json()) // Middleware to parse JSON bodies
+app.use(express.text()) // Middleware to parse text bodies
+app.use(express.urlencoded({ extended: true })) // Middleware to parse URL-encoded bodies
 
-app.get('/', (req: Request, res: Response) => {
-  // res.send('Hello World!')
-  res.status(200).json({
-    message: 'Hello World!'
-  })
-})  
-
-app.post('/', async (req: Request, res: Response) => {
-    console.log(req.body);
-
+// Database connection configuration
+const pool = new Pool({
+  connectionString:"postgresql://neondb_owner:npg_6IGdWECxQF4n@ep-tiny-block-al69d9sb-pooler.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 })
 
+// Endpoint to handle GET requests
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})  
+
+// Endpoint to handle POST requests
+app.post('/', async (req: Request, res: Response) => {
+    // console.log(req.body);
+    const {name,age} = req.body;
+    res.status(201).json({
+        message: 'Data received successfully',
+        data: {name,age}
+    });
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`This app is running on port ${port}`)
 })
